46a465fcc275bc7fc2458c3ad183e26767e8d761
Jayant Baid <baidjayant2002@gmail.com>
2024-05-25 12:57:29 +0530

    first component is created

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,8 +1,12 @@
+import Chai from "./Chai";
 function App() {
   return (
-    <>
+    <> 
+    {/* <></> : fragment, return this single one */}
       <h1>Jayant Baid | Chai Aur React</h1>
       <p>Creating react app using vite</p>
+
+      <Chai/>
     </>
   );
 }
