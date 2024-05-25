9ccd4697ad96bf6a5bc88fd22828808c546c0604
Jayant Baid <baidjayant2002@gmail.com>
2024-05-25 12:21:18 +0530

    Created 2 react apps using npx and vite

---
+++ 01_basic_react_using_npx/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <>
+    <h1>Jayant Baid | Chai Aur React</h1>
+    <ul>
+      <li>Create react app using npx (node pakage executor)</li>
+      <li>Create react app using vite(lightweight)</li>
+    </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Jayant Baid | Chai Aur React</h1>
+      <p>Creating react app using vite</p>
+    </>
+  );
+}
+
+export default App;
