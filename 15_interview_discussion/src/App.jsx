39cff023b2eb84a1d097d97b93feefac9aa8026c
Jayant Baid <baidjayant2002@gmail.com>
2024-07-18 13:25:30 +0530

    Interview Discussion 2

--- 15_interview_discussion/src/App.jsx
+++ 15_interview_discussion/src/App.jsx
@@ -2,19 +2,37 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState(1);
-  // const [multiplyValue, setMultiplyValue] = useState(1)
-  let multiplyValue = value * 5;
+  // When we use useState, if there is a change in the state, then the whole component will get re-mounted/re-render.
+  // If same value is given, then no re-render
 
-  const multiplyBy5 = () => {
-    // setMultiplyValue(value*5)
-    setValue(value + 1);
+  console.log("App rendered", Math.random());
+  // const [value, setValue] = useState(1);
+  const [value, setValue] = useState({
+    value: 0,
+  });
+  // const [multiplyValue, setMultiplyValue] = useState(1)
+  // let multiplyValue = value * 5;
+
+  // const multiplyBy5 = () => {
+  //   // setMultiplyValue(value*5)
+  //   setValue(value + 1);
+  // };
+  const clickMe = () => {
+    console.log("logged");
+    //  setValue(value+1) // re-render
+    //  setValue(1) // No re-render, when useState value is same as of setvalue value
+    //  setValue(3) // 2 re-render, then no re-render
+
+    setValue({
+      value: 0,
+    }); // re-render hoga
   };
+
   return (
     <>
-      <h1>Main Value: {value}</h1>
-      <button onClick={multiplyBy5}>Click to multiply by 5</button>
-      <h2>Multiplied Value: {multiplyValue}</h2>
+      <h1>Main Value: {value.value}</h1>
+      <button onClick={clickMe}>Click to multiply by 5</button>
+      {/* <h2>Multiplied Value: {multiplyValue}</h2> */}
     </>
   );
 }
